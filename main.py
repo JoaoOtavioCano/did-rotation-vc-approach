8bbd6f4a49911ef138ac574ee8fa0fc3035c25ac
João Otávio Cano <joaootaviocano@gmail.com>
2024-11-10 19:53:07 -0300

    credential template done

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import argparse
+import asyncio
+import json
+import didkit
+import datetime
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("echo", help="echo the string you use here")
+    parser.add_argument("old_did", help="DID of the old method")
+    parser.add_argument("new_did", help="DID of the new method")
     args = parser.parse_args()
-    print(args.echo)
+    print(f"old: {args.old_did}\nnew: {args.new_did}")
+
+    issuance_date = datetime.datetime.utcnow().isoformat() + "Z"
+    credential = {
+        "@context": "https://www.w3.org/2018/credentials/v1",
+        "type": ["VerifiableCredential", "DIDRotationCredential"],
+        "issuer": f"{args.old_did}",
+        "issuanceDate": f"{issuance_date}",
+        "credentialSubject": {
+            "id": f"{args.new_did}",
+            "sameControllerAs": f"{args.old_did}",
+        },
+    }
+
+    print(json.dumps(credential, indent=2))
+
+
 
 if __name__ == '__main__':
     main()
